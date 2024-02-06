246dfb7239e4b1b6a1a6532bc85158910f1e96b9
whs <hswongac@gmail.com>
2024-02-06 12:05:26 +0800

    First commit

---
+++ nix/local/packages.nix
@@ -0,0 +1,18 @@
+with inputs.nixpkgs; let
+  hermes_1_8_0 = stdenv.mkDerivation {
+    name = "hermes";
+    version = "1.8.0";
+    src = fetchurl {
+      url = "https://github.com/informalsystems/hermes/releases/download/v1.8.0/hermes-v1.8.0-aarch64-unknown-linux-gnu.tar.gz";
+      hash = "sha256-uQ6VKXJXK/63jXWSVU61Bk0+DZbH+f7cnHg+lKNQE0Y=";
+    };
+    dontBuild = true;
+    sourceRoot = ".";
+    installPhase = ''
+      mkdir -p $out/bin
+      cp hermes $out/bin/hermes
+    '';
+  };
+in {
+  inherit hermes_1_8_0;
+}
