e27d1572ff88f510461a278eb6ab07356240b969
whs <hswongac@gmail.com>
2024-03-27 12:52:56 +0800

    Update

--- nix/local/packages.nix
+++ nix/local/packages.nix
@@ -33,7 +33,18 @@ with inputs.nixpkgs; let
     CARGO_BUILD_TARGET = target;
     CARGO_BUILD_RUSTFLAGS = "-Ctarget-feature=+crt-static";
   };
+  hermes_1_8_2 = rustPlatform.buildRustPackage rec {
+    pname = "hermes";
+    version = "1.8.2";
+    src = fetchTarball {
+      url = "https://github.com/informalsystems/hermes/archive/refs/tags/v${version}.tar.gz";
+      sha256 = "sha256:0m201mr5if71p4vj340i2cxn77y0778g1q3p923ilg3fr5czp0zn";
+    };
+    doCheck = false;
+    cargoHash = "sha256-CwU9VTAPo9hm5JGzt1XzCLGWbHFlie/+jhZ5ckWo2qU=";
+  };
 in {
   inherit hermes_1_8_0;
+  inherit hermes_1_8_2;
   inherit hermes_gas_estimation;
 }
